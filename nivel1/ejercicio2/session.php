3bda2b8f2c528a3bcb5a2f7023734cd82fe9a4e4
malmental <orlandorojasoyarzun@gmail.com>
2026-01-21 16:10:40 +0100

    validaciones implementadas

--- nivel1/ejercicio2/session.php
+++ nivel1/ejercicio2/session.php
@@ -1,21 +1,49 @@
-<?php 
+<?php
 session_start();
 
-if (!empty($_POST["Nombre"]) && !empty($_POST["Email"]) && !empty($_POST["Edad"]))
-{
-    $nombre = $_POST ["Nombre"];
-    $email = $_POST ["Email"];
-    $edad = $_POST ["Edad"];
+require_once 'Validaciones.php';
 
-    $_SESSION ["Nombre"] = $nombre;
-    $_SESSION ["Email"] = $email;
-    $_SESSION ["Edad"] = $edad;
 
-    echo 'Usuario: ' . $nombre . PHP_EOL .
-        'Email: ' . $email . PHP_EOL . 
-        'Edad: ' . $edad . PHP_EOL;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $todosLosErrores = [];
+
+    $resultadoNombre = validadorNombre($_POST["Nombre"]);
+    if (!$resultadoNombre['valido']) {
+        foreach ($resultadoNombre['errores'] as $error) {
+            $todosLosErrores[]= $error;
+        }
+    }
+
+    $resultadoEmail = validadorEmail($_POST["Email"]);
+    if (!$resultadoEmail['valido']) {
+        foreach ($resultadoEmail['errores'] as $error) {
+            $todosLosErrores[] = $error;
+        }
+    }
+
+    $resultadoEdad = validadorEdad($_POST["Edad"]);
+    if (!$resultadoEdad['valido']) {
+        foreach ($resultadoEdad['errores'] as $error) {
+            $todosLosErrores[] = $error;
+        }
+    }
     
+    // Hay algun error?
+    if (count($todosLosErrores) > 0) {
+        echo 'Errores encontrados: ' . PHP_EOL;
+        foreach ($todosLosErrores as $error) {
+            echo $error;
+        }
+    } else {
+        $_SESSION["Nombre"] = $resultadoNombre['valor'];
+        $_SESSION["Email"] = $resultadoEmail['valor'];
+        $_SESSION["Edad"] = $resultadoEdad['valor'];
+        
+        echo 'Nombre: ' . $_SESSION["Nombre"] . PHP_EOL;
+        echo 'Email: ' . $_SESSION["Email"] . PHP_EOL;
+        echo 'Edad: ' . $_SESSION["Edad"] . PHP_EOL;
+    }
 } else {
-    echo "Los campos no pueden estar vacios";
+    echo 'Acceso no valido';
 }
-
